f49ba621b8a80abafa874bac71ef1f99b96bb4c1
aymanecoder <aymanbiti2@gmail.com>
2023-02-11 17:43:26 +0100

    add AuthController and generate private key

---
+++ src/main/java/com/project/springsecurity/security/RsakeysConfig.java
@@ -0,0 +1,13 @@
+package com.project.springsecurity.security;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+
+
+@ConfigurationProperties(prefix = "rsa")
+public record RsakeysConfig(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+}
--- src/main/java/com/project/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/project/springsecurity/security/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.project.springsecurity.security;
 
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -16,24 +31,47 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig  {
+    private final RsakeysConfig rsakeysConfig;
+
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("user2").password("{noop}1234").authorities("USER").build(),
+                User.withUsername("admin").password("{noop}1234").authorities("USER","ADMIN").build()
+
+
+        );
+
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(null);
-        http
-                .formLogin()
-                .and()
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-        return http.build();
+//        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//        authenticationManagerBuilder.authenticationProvider(null);
+       return http
+               .csrf(csrf->csrf.disable())
+               .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+               .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+               .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .httpBasic(Customizer.withDefaults())
+               .build();
+    }
+    @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(rsakeysConfig.publicKey()).build();
+    }
+    @Bean
+    JwtEncoder jwtEncoder(){
+        JWK jwk= new RSAKey.Builder(rsakeysConfig.publicKey()).privateKey(rsakeysConfig.privateKey()).build();
+        JWKSource<SecurityContext> jwkSource= new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
     }
 
 
 
-}
+
+    }
 
 
